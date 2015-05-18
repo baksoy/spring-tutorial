968a70b78255b27fa6fcc269c0565b44d2cb6a29
baksoy <baksoy@gmail.com>
2015-05-18 13:59:31 -0400

    Adding VehicleModelUpdate

--- project/src/main/java/com/thinkful/spring/controller/VehicleController.java
+++ project/src/main/java/com/thinkful/spring/controller/VehicleController.java
@@ -1,5 +1,6 @@
 package com.thinkful.spring.controller;
 
+
 import com.google.common.base.Preconditions;
 import com.thinkful.contract.dto.VehicleDto;
 import com.thinkful.contract.dto.VehicleModelDto;
@@ -18,8 +19,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 public class VehicleController {
@@ -37,6 +40,7 @@ public class VehicleController {
     public HttpEntity<List<VehicleDto>> getAllVehicles() {
         List<Vehicle> vehicles = vehicleService.findAllVehicles();
         List<VehicleDto> vehicleDtos = mapper.mapAsList(vehicles,VehicleDto.class);
+
 
         return new ResponseEntity<List<VehicleDto>>(vehicleDtos, HttpStatus.OK);
     }
@@ -58,30 +62,36 @@ public class VehicleController {
     public HttpEntity<VehicleDto> updateVehicle(@RequestBody VehiclePersistenceRequest vehiclePersistenceRequest) {
         Preconditions.checkArgument(vehiclePersistenceRequest != null);
 
+
         Vehicle vehicleEntity = vehicleService.findVehicleById(vehiclePersistenceRequest.getId());
         Preconditions.checkArgument(vehicleEntity != null, "No matching vehicle found");
+
 
         if (vehicleEntity.getModel().getId() != vehiclePersistenceRequest.getModelId()) {
             VehicleModel vehicleModel = vehicleModelService.findById(vehiclePersistenceRequest.getModelId());
             Preconditions.checkArgument(vehicleModel != null, "No matching vehicle model found");
 
+
             vehicleEntity.setModel(vehicleModel);
         }
 
+
         vehicleEntity.setColor(vehiclePersistenceRequest.getColor());
+
 
         Vehicle updatedVehicleEntity = vehicleService.updateVehicle(vehicleEntity);
         VehicleDto updatedVehicleDto = mapper.map(updatedVehicleEntity, VehicleDto.class);
 
+
         return new ResponseEntity<VehicleDto>(updatedVehicleDto, HttpStatus.OK);
     }
-
-//    public HttpEntity<VehicleModelDto> updateVehicleModel
 
     @RequestMapping(value = "/vehicle-models",method = RequestMethod.GET)
     public HttpEntity<List<VehicleModelDto>> getAllVehicleModels() {
 
+
         List<VehicleModel> vehicleModels = vehicleModelService.findAllVehicleModels();
+
 
 //        List<VehicleModelDto> vehicleModelDtos = new ArrayList<VehicleModelDto>();
 //        for (VehicleModel vehicleModel: vehicleModels) {
@@ -91,6 +101,7 @@ public class VehicleController {
 //            vehicleModelDtos.add(vehicleModelDto);
 //        }
         List<VehicleModelDto> vehicleModelDtos = mapper.mapAsList(vehicleModels,VehicleModelDto.class);
+
 
         return new ResponseEntity<List<VehicleModelDto>>(vehicleModelDtos, HttpStatus.OK);
     }
@@ -103,5 +114,20 @@ public class VehicleController {
         VehicleModelDto vehicleModelDto = mapper.map(vehicleModelEntity,VehicleModelDto.class);
 
         return new ResponseEntity<VehicleModelDto>(vehicleModelDto, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/vehicle-model", method=RequestMethod.PUT)
+    public HttpEntity<VehicleModelDto> updateVehicleModel(@RequestBody VehicleModelDto vehicleModelRequest){
+        Preconditions.checkArgument(vehicleModelRequest != null);
+
+        VehicleModel vehicleModelEntity = vehicleModelService.findById(vehicleModelRequest.getId());
+        Preconditions.checkArgument(vehicleModelEntity != null, "No matching vehicle model found");
+
+        vehicleModelEntity.setName(vehicleModelRequest.getName());
+
+        VehicleModel updateVehicleModelEntity = vehicleModelService.updateVehicleModel(vehicleModelEntity);
+        VehicleModelDto updateVehicleModelDto = mapper.map(updateVehicleModelEntity, VehicleModelDto.class);
+
+        return new ResponseEntity<VehicleModelDto>(updateVehicleModelDto, HttpStatus.OK);
     }
 }
