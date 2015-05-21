1c0da926e849981995e19e727955539d6ea4f9a7
baksoy <baksoy@gmail.com>
2015-05-21 15:56:49 -0400

    Adding Maintenance CRUD

---
+++ project/src/main/java/com/thinkful/spring/dao/MaintenanceDao.java
@@ -0,0 +1,7 @@
+package com.thinkful.spring.dao;
+
+
+import com.thinkful.spring.entity.Maintenance;
+
+public interface MaintenanceDao extends CrudDao<Maintenance, Integer> {
+}
---
+++ project/src/main/java/com/thinkful/spring/service/MaintenanceService.java
@@ -0,0 +1,16 @@
+package com.thinkful.spring.service;
+
+
+import com.thinkful.spring.entity.Maintenance;
+
+import java.util.List;
+
+public interface MaintenanceService {
+   Maintenance createMaintenance (String maintenanceItem);
+   Maintenance updateMaintenance (String maintenanceItem);
+   Maintenance findById(int id);
+   List<Maintenance> findAllMaintenanceItems;
+   void deleteMaintenanceItem(int maintenanceId);
+}
+
+
